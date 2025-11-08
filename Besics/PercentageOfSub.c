08177079db23037619a990b47756cb00104d4fe4
SajidSojib <sajidahmed2401@gmail.com>
2025-11-08 23:27:12 +0600

    percetage of subject

---
+++ Besics/PercentageOfSub.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    // float P=95,C=91,M=83,E=80,B=66;
+    // float x = (P+C+M+E+B)/5;
+
+    // printf ("The percentage is: %f",x);
+
+    float x1=39, x2=29, x3=33,x4=37, X;
+    X = (x1+x2+x3+x4)*100/(40*4);
+    printf("Parcentage: %f",X);  
+     
+    return 0;
+
+}
