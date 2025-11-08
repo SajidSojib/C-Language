0956a0d9393d138f71934774526fdca76d317994
SajidSojib <sajidahmed2401@gmail.com>
2025-11-08 23:23:18 +0600

    printing variable

---
+++ Besics/PrintingVariable.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+int x,y;
+printf ("First number: ");
+  scanf("%d",&x);
+printf ("Second Number: ");
+ scanf("%d",&y);
+
+
+  printf ("%d\n",x+y);
+  printf ("%d\n",x-y);
+  printf ("%d\n",x*y);
+  printf ("%d\n",x/y);
+
+x=x+9;
+  printf ("\n%d",x);
+
+
+return 0;
+}
+
