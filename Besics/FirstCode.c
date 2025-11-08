fb3e2c0e5f14de036662b40d8734e473906dd218
SajidSojib <sajidahmed2401@gmail.com>
2025-11-08 23:22:05 +0600

    float datatype

---
+++ Besics/FirstCode.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hellow World");
+    printf("Sajid Ahmed\n");
+    return 0;
+}
---
+++ Besics/Float.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    
+    float x = 7,y = 2;
+
+    printf ("\n%f",7/2);  //  Need to use %d
+    printf ("\n%f",7.0/2);
+    printf ("\n%f",x/y);
+
+    return 0;
+}
