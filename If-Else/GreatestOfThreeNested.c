9494fb82865631cd4695a6b8b72425d7654a1d7d
SajidSojib <sajidahmed2401@gmail.com>
2025-11-25 22:27:10 +0600

    nested if else

---
+++ If-Else/GreatestOfThreeNested.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+    int  x, y, z;
+    printf("Enter first number: ");
+    scanf("%d", &x);
+    printf("Enter second number: ");
+    scanf("%d",&y);
+    printf("Enter third number: ");
+    scanf("%d", &z);
+    if (x>y)
+    {
+        if (x>z)
+        {
+            printf ("%d is the greatest",x);
+        }
+        else{
+          printf ("%d is the greatest",z);   
+        }
+         
+    }
+    else{ if (y>z)
+    {
+        printf ("%d is the greatest",y);
+        }
+        else{
+          printf ("%d is the greatest",z);    
+    }
+    }
+    return 0;
+}
