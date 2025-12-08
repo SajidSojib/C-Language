5f07b29a4b841b08a139651b549442c09450dd1d
SajidSojib <sajidahmed2401@gmail.com>
2025-12-08 23:36:47 +0600

    print odd numbers without math

---
+++ Loop/APwithoutMath.c
@@ -0,0 +1,17 @@
+//4 7 10...n
+
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter a number: ");
+    scanf ("%d",&n);
+
+    int a=1;
+    for(int i=1;i<=n;i++){
+        printf ("%d ",a);
+        a=a+2;
+    }
+
+
+    return 0;
+}
