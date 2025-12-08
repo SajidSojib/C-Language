3cd86e486a46da534e2642a416aca5b9eabc44e7
SajidSojib <sajidahmed2401@gmail.com>
2025-12-08 23:57:14 +0600

    do while

---
+++ Loop/DoWhile.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int i = 1;
+    while (i < 4)
+    {
+        printf("hellow\n");
+        i++;
+    }
+    
+    do
+    {
+        printf("world");
+    } while (i=0);
+    
+    return 0;
+}
