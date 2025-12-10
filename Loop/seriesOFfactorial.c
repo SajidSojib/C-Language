e750d59636f9c05c72fc6a1cf8cc91d9a93432f6
SajidSojib <sajidahmed2401@gmail.com>
2025-12-10 23:46:34 +0600

    print series of factorial

---
+++ Loop/seriesOFfactorial.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+
+    int n, factorial = 1 ;
+
+    printf("enter a number: ");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        factorial = factorial * i;
+         printf("%d\n", factorial);
+    }
+   
+
+    return 0;
+}
